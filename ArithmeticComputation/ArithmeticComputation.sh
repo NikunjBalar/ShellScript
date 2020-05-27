a09df3faa2570d0284ebbd0acd6c8749bd0e9495
9737366556 <kunj.hasi103@gmail.com>
2020-05-27 18:43:53 +0530

    ADDED: all shellScripting programs in single repository

--- ArithmeticComputation/ArithmeticComputation.sh
+++ ArithmeticComputation/ArithmeticComputation.sh
@@ -1,34 +1,45 @@
 #!/bin/bash -x
+
+#Aurthor : Nikunj Balar
+#Purpose : syntex for arithmetic computation
+
+#Read input from user
 echo "ENTER THREE INPUT"
 read -p "enter the value..>" a
 read -p "enter the value..>" b
 read -p "enter the value..>" c
 
+#declare Dictionary 
 declare -A result
 
+#Syntex for Arithmetic computation
 compute0=$(( $a + $b * $c))
 compute1=$(( $a * $b + $c ))
 compute2=$(( $c + $a / $b ))
 compute3=$(( $a % $b + $c ))
 
+#Store element into the dictionary
 result["compute0"]=$compute0
 result["compute1"]=$compute1
 result["compute2"]=$compute2
 result["compute3"]=$compute3
 
-
+#Declaring and initallising Array
 for (( i=0 ; i<4; i++))
 do
         resultArr[$i]="${result[compute$i]}"
 done
+# get all the element of the dictionary
 echo ${result[@]}
 
+# sort array into asscending order
 echo "asscending array"
 for asc in ${!resultArr[@]}
 do
         echo ${resultArr[$asc]}
 done | sort -n
 
+#sort array into decending order
 echo "decending  array"
 for dec in ${!resultArr[@]}
 do
--- GamblingSimulatorProblame/gamblingSimulator.sh
+++ GamblingSimulatorProblame/gamblingSimulator.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash 
+
+#Aurthor : Nikunj Balar
+#Purpose : For GamblingSimulstor problem
 echo "WELCOME TO GAMBLING SIMULATOR "
 
 #Constant
@@ -13,9 +16,10 @@ DAY=20
 cash=0
 totalCash=0
 
-
+#declare the dictionary
 declare -A storeWinloss
 
+#function for each day gambling for a month
 function dailyPlay(){
 
         for ((day=1 ;day<=$DAY; day++))
@@ -31,14 +35,16 @@ function dailyPlay(){
                                 stake=$(( $stake+$BET))
                         fi
                 done
-
+		#calculating each day cash
                 cash=$(($stake - $STAKE))
+		#calculating total cash at the end of the day
                 totalCash=$(($totalCash + $cash))
+		#storing the value of the per day cash and total cash at the end of the day
                 storeWinLoss[$day]="$cash  $totalCash"
                 echo  "Day$day"     "${storeWinLoss[$day]}"
         done
 }
-
+#function for find luckiest day in the month
 function findLuckiestDay(){
 
         echo "Luckiest day in month..."
@@ -48,6 +54,7 @@ function findLuckiestDay(){
         done | sort -k3 -rn | head -1
 }
 
+#Function for find unluck day in the month
 function findUnLuckiestDay(){
 
         echo "Unluckiest day in month.."
@@ -57,23 +64,27 @@ function findUnLuckiestDay(){
         done | sort -k3 -n | head -1
 }
 
+#function for checking the possibility for gamblinf of the next month
 function checkNextMonthForGambling(){
 
         if [[ $totalCash -gt 0 ]]
         then
                 echo " "
+		#calling main function
                  main
         else
                 echo "next time play......."
         fi
 }
 function main(){
-
+	#calling functions
         dailyPlay
         findLuckiestDay
         findUnLuckiestDay
         checkNextMonthForGambling
 }
+
+#calling main function
 main
 
 
